768dd98586fc4bb27e1898808cbd0e69519936bb
Adrian Van Nierop <adrian.vannierop@colensobbdo.co.nz>
2021-05-24 15:43:31 +1200

    test

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,10 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = (): JSX.Element => {
-  var blahaha;
-  var aahawa;
-  var wahtasdfasw;
-
   return (
     <div className="App">
       <header className="App-header">
