a6fd9172b50c0941c7d23829fe9d0123ca7fb65c
Adrian Van Nierop <adrian.vannierop@colensobbdo.co.nz>
2021-05-24 15:29:47 +1200

    added error to App.tsx

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = (): JSX.Element => {
+  var blahaha;
+
   return (
     <div className="App">
       <header className="App-header">
